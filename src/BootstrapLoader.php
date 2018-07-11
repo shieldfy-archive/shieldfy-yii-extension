17940077e2aec02463764b51383b10c99e966934
Eslam Salem <netcodez@gmail.com>
2018-07-11 17:13:44 +0200

    init

--- src/BootstrapLoader.php
+++ src/BootstrapLoader.php
@@ -3,6 +3,7 @@ namespace Shieldfy\Extensions\Yii;
 
 use yii\base\BootstrapInterface;
 use yii\base\Application;
+use Shieldfy\Guard;
 
 /**
  * https://www.yiiframework.com/doc/guide/2.0/en/structure-extensions#creating-extensions
@@ -11,8 +12,14 @@ class BootstrapLoader implements BootstrapInterface
 {
     public function bootstrap($app)
     {
-        $app->on(Application::EVENT_BEFORE_REQUEST, function () {
-            echo "Hi Yii";
+        $app->on(Application::EVENT_BEFORE_REQUEST, function () use($app) {
+            $shieldfy = \Shieldfy\Guard::init([
+                'endpoint'      => 'https://api.shieldfy.com/v1',
+                'app_key'       => '',
+                'app_secret'    => ''
+            ]);
+
+            \Yii::$container->set('shieldfy',$shieldfy);
         });
     }
 }
