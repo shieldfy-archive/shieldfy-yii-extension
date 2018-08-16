2b4a5226e7d242fe78554a00e471b276ed91728b
Karim Mohamed <komicho1996@gmail.com>
2018-08-16 19:08:28 +0200

    Support components

--- src/BootstrapLoader.php
+++ src/BootstrapLoader.php
@@ -10,20 +10,19 @@ use Shieldfy\Guard;
  */
 class BootstrapLoader implements BootstrapInterface
 {
+    public $APP_KEY;
+    public $APP_SECRET;
+
     public function bootstrap($app)
     {
         $app->on(Application::EVENT_BEFORE_REQUEST, function () use($app) {
             if (php_sapi_name() === "cli") {
                 return;
             }
-            // get Shieldfy Params
-            $params = \Yii::$app->params;
-            $paramsShieldfyExists = array_key_exists('Shieldfy', $params);
-            // init Shieldfy Guard
             $shieldfy = \Shieldfy\Guard::init([
                 'endpoint'      => 'https://api.shieldfy.com/v1',
-                'app_key'       => $paramsShieldfyExists ? $params['Shieldfy']['appKey'] : '',
-                'app_secret'    => $paramsShieldfyExists ? $params['Shieldfy']['appSecret'] : ''
+                'app_key'       => $this->APP_KEY,
+                'app_secret'    => $this->APP_SECRET
             ]);
 
             \Yii::$container->set('shieldfy',$shieldfy);
